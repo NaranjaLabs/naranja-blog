f1e9580559d382288083085c7b702ff14140ed5c
Vitor Veloso <vitor.asv@gmail.com>
2023-04-21 12:03:12 -0300

    Adding Prettier ignore, and Axios

--- app/layout.tsx
+++ app/layout.tsx
@@ -12,7 +12,10 @@ export default function RootLayout({
 }) {
   return (
     <html lang="pt-br">
-      <body>{children}</body>
+      <body>
+        <header>Olar</header>
+        <main>{children}</main>
+      </body>
     </html>
   );
 }
