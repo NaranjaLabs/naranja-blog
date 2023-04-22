ccbb320f26fffa0b6d80a6eac8baaa84281ac3a7
Mateus Meneses <mateusppa10@gmail.com>
2023-04-21 22:15:22 -0300

    :constructor: Criando preview mode

--- app/(user)/page.tsx
+++ app/(user)/page.tsx
@@ -1,15 +1,25 @@
+// import { previewData } from 'next/dist/client/components/headers';
+
+import { previewData } from 'next/headers';
+
 import Article from '@components/components/Article';
 import MainArticle from '@components/components/MainArticle';
 
 export default function Blog() {
-  return (
-    <main className="max-w-6xl px-10 m-auto mt-12 place-items-center">
-      <MainArticle />
-      <div className="mt-16 grid sm:grid-cols-2 lg:grid-cols-3 gap-14">
-        <Article />
-        <Article />
-        <Article />
-      </div>
-    </main>
-  );
+  console.log(previewData());
+
+  if (previewData()) {
+    return <div>Preview Mode</div>;
+  } else {
+    return (
+      <main className="max-w-6xl px-10 m-auto mt-12 place-items-center">
+        <MainArticle />
+        <div className="mt-16 grid sm:grid-cols-2 lg:grid-cols-3 gap-14">
+          <Article />
+          <Article />
+          <Article />
+        </div>
+      </main>
+    );
+  }
 }
