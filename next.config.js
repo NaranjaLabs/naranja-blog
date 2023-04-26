04e183a983301987208060524bfe4fce7b09a46f
Vitor Veloso <vitor.asv@gmail.com>
2023-04-26 14:22:32 -0300

    Integration with API in progress

--- next.config.js
+++ next.config.js
@@ -3,6 +3,17 @@ const nextConfig = {
   experimental: {
     appDir: true,
   },
+  images: {
+    formats: ['image/avif', 'image/webp'],
+    remotePatterns: [
+      {
+        protocol: 'http',
+        hostname: '127.0.0.1',
+        port: '1337',
+        pathname: '/uploads/**',
+      },
+    ],
+  },
 }
 
 module.exports = nextConfig
