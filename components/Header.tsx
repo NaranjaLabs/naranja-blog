3672cf36b47cabf949894425a84c35d7213ea2d9
Mateus <mateusppa10@gmail.com>
2023-05-11 20:28:32 -0300

    :lipstick: logo link home page

--- components/Header.tsx
+++ components/Header.tsx
@@ -6,7 +6,9 @@ export default function Header() {
     <header className="w-full h-14 bg-[#fb7021] grid place-items-center ">
       <div className="max-w-[1072px] w-full flex items-center text-slate-100 justify-between">
         <div className="flex items-center">
-          <Image src="/logo-vector.svg" width={56} height={56} alt="logo" />
+          <Link href="/">
+            <Image src="/logo-vector.svg" width={56} height={56} alt="logo" />
+          </Link>
           <h3 className=" ml-2 text-xl">NaranjaLabs</h3>
         </div>
         <div className="flex gap-6 mr-2">
