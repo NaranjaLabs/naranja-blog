122601989bcd1860f54138000a3675a663fd39c3
Mateus Meneses <mateusppa10@gmail.com>
2023-05-12 21:54:18 -0300

    :construction: Testando revalidação em prod

--- pages/api/revalidate.ts
+++ pages/api/revalidate.ts
@@ -4,11 +4,17 @@ export default async function revalidate(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
+  const paths = [`/post/${req.query.slug}`, '/'];
   try {
-    await res.revalidate('/');
-    await res.revalidate(`/post/${req.query.slug}`);
+    console.log(req.query.slug);
+    for (const path of paths) {
+      console.log(path);
+
+      await res.revalidate(path);
+    }
+    // await res.revalidate(`/post/${req.query.slug}`);
     const message = `Updated route: /blog/${req.query.slug}`;
-    return res.status(200).json({ message });
+    return res.json({ message });
   } catch (err: any) {
     console.log(err);
     return res.status(500).json({ message: err.message });
